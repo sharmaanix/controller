159b2e633562bc72e2e1621193607e82c154dc24
Ali Husnain <78292002+aliwtw@users.noreply.github.com>
2023-11-27 17:31:36 +1100

    add more functions

---
+++ nsl KDD/run.py
@@ -0,0 +1,103 @@
+import pandas as pd
+
+
+# get the training file
+full_training_set = 'KDDTrain+.txt'
+
+#read data file 
+df = pd.read_csv(full_training_set)
+
+# add the column labels
+columns = (['duration'
+,'protocol_type'
+,'service'
+,'flag'
+,'src_bytes'
+,'dst_bytes'
+,'land'
+,'wrong_fragment'
+,'urgent'
+,'hot'
+,'num_failed_logins'
+,'logged_in'
+,'num_compromised'
+,'root_shell'
+,'su_attempted'
+,'num_root'
+,'num_file_creations'
+,'num_shells'
+,'num_access_files'
+,'num_outbound_cmds'
+,'is_host_login'
+,'is_guest_login'
+,'count'
+,'srv_count'
+,'serror_rate'
+,'srv_serror_rate'
+,'rerror_rate'
+,'srv_rerror_rate'
+,'same_srv_rate'
+,'diff_srv_rate'
+,'srv_diff_host_rate'
+,'dst_host_count'
+,'dst_host_srv_count'
+,'dst_host_same_srv_rate'
+,'dst_host_diff_srv_rate'
+,'dst_host_same_src_port_rate'
+,'dst_host_srv_diff_host_rate'
+,'dst_host_serror_rate'
+,'dst_host_srv_serror_rate'
+,'dst_host_rerror_rate'
+,'dst_host_srv_rerror_rate'
+,'attack'
+,'level'])
+
+df.columns = columns
+
+#set attack flag
+is_attack = df.attack.map(lambda a: 0 if a == 'normal' else 1)
+df['attack_flag'] = is_attack
+
+#check attack type
+
+# lists to hold our attack classifications
+dos_attacks = ['apache2','back','land','neptune','mailbomb','pod','processtable','smurf','teardrop','udpstorm','worm']
+probe_attacks = ['ipsweep','mscan','nmap','portsweep','saint','satan']
+privilege_attacks = ['buffer_overflow','loadmdoule','perl','ps','rootkit','sqlattack','xterm']
+access_attacks = ['ftp_write','guess_passwd','http_tunnel','imap','multihop','named','phf','sendmail','snmpgetattack','snmpguess','spy','warezclient','warezmaster','xclock','xsnoop']
+
+# we will use these for plotting below
+attack_labels = ['Normal','DoS','Probe','Privilege','Access']
+
+# helper function to pass to data frame mapping
+def map_attack(attack):
+    if attack in dos_attacks:
+        # dos_attacks map to 1
+        attack_type = 1
+    elif attack in probe_attacks:
+        # probe_attacks mapt to 2
+        attack_type = 2
+    elif attack in privilege_attacks:
+        # privilege escalation attacks map to 3
+        attack_type = 3
+    elif attack in access_attacks:
+        # remote access attacks map to 4
+        attack_type = 4
+    else:
+        # normal maps to 0
+        attack_type = 0
+        
+    return attack_type
+
+# map the data and join to the data set
+attack_map = df.attack.apply(map_attack)
+df['attack_map'] = attack_map
+
+
+attack_vs_protocol = pd.crosstab(df.attack, df.protocol_type)
+
+
+#check
+print(df.head())
+
+print(attack_vs_protocol)
